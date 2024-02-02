b941e039c8b0844cce884191c790f1533795c05e
Vinay <vinaybharatha555@gmail.com>
2024-02-02 17:19:37 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Springboot_Redis_Cloud_App
---
+++ src/main/java/in/ashokit/rest/StudentRestController.java
@@ -0,0 +1,29 @@
+package in.ashokit.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import in.ashokit.entity.Student;
+import in.ashokit.repo.StudentRepository;
+
+@RestController
+public class StudentRestController {
+	
+	@Autowired
+	private StudentRepository srepo;
+	
+	@GetMapping("/student")
+	public Iterable<Student> getAllStudents(){
+		return srepo.findAll();
+	}
+	
+	
+	@PostMapping("/student")
+	public String addStudent(@RequestBody Student student) {
+		srepo.save(student);
+		return "Student Saved";
+	}
+}
